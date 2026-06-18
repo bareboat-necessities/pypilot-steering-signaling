b13a8418aec548f64816e88a9a1a73ab668b2213
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-18 15:14:48 -0400

    Export servo protocol bridge API

--- src/pypilot_steering_signaling.hpp
+++ src/pypilot_steering_signaling.hpp
@@ -4,3 +4,4 @@
 #include "pypilot_steering_signaling/rudder_runtime.hpp"
 #include "pypilot_steering_signaling/servo_command.hpp"
 #include "pypilot_steering_signaling/servo_safety.hpp"
+#include "pypilot_steering_signaling/servo_protocol_bridge.hpp"
