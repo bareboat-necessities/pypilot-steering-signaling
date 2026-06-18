fdb154efa28a8d37409988a740cdbd1c2ae8ce99
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-18 15:30:38 -0400

    Export servo configuration helpers

--- src/pypilot_steering_signaling.hpp
+++ src/pypilot_steering_signaling.hpp
@@ -6,3 +6,4 @@
 #include "pypilot_steering_signaling/servo_safety.hpp"
 #include "pypilot_steering_signaling/servo_protocol_bridge.hpp"
 #include "pypilot_steering_signaling/servo_runtime.hpp"
+#include "pypilot_steering_signaling/servo_config_commands.hpp"
